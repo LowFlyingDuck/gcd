bc8044d795e1ca7069e68a6ba40bcd68ea0bdf3e
Nils Schwebel <datateam@gmx.de>
2021-12-03 05:33:31 +0100

    first commit

--- findgcd.js
+++ findgcd.js
@@ -1,4 +1,11 @@
 // recursive=
 const gcd = (a, b) => (a % b === 0) ? a >= b ? b : a : (console.log(a + ' = ' + b + ' * ' + Math.floor(a/b) + ' + ' + a%b), gcd(b, a % b));
 
-console.log(gcd(2322, 654));
+const encrypt = (e, n, m) => m**e % n;
+const decrypt = (c, d) => c**d;
+
+const p = 43;
+const q = 53;
+const e = 7;
+
+console.log(gcd((p-1)*(q-1), e));
