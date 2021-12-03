2d661014ee1e1e07cc97a779c48b03828cac16bf
Nils Schwebel <datateam@gmx.de>
2021-12-03 06:00:19 +0100

    added proper ciphers

--- findgcd.js
+++ findgcd.js
@@ -1,13 +1,13 @@
 // recursive=
 const gcd = (a, b) => (a % b === 0) ? a >= b ? b : a : (console.log(a + ' = ' + b + ' * ' + Math.floor(a/b) + ' + ' + a%b), gcd(b, a % b));
 
-const encrypt = (e, n, m) => m**e % n;
-const decrypt = (c, d) => c**d;
+const encrypt = (m, e, n) => BigInt(m)**BigInt(e) % BigInt(n);
+const decrypt = (c, d, n) => BigInt(c)**BigInt(d) % BigInt(n);
 
-const getD = (p, q, e) => { let i = 0; while((i*e) % ((p-1)*(q-1)) !== 1) i++; return i; } 
+const getD = (p, q, e) => { let i = 0; console.log('1 = x ' + ' * ' + e + ' mod ' + (p-1)*(q-1)); while((i*e) % ((p-1)*(q-1)) !== 1) i++; return i; } 
 
-const p = 43;
-const q = 53;
-const e = 11;
+const p = 37;
+const q = 31;
+const e = 7;
 
-console.log(getD(p, q, e));
+console.log(decrypt(917, 463, p*q));
